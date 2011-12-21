8911cb7342115dc5eee57044958326aca9573118
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2011-12-21 23:21:26 +0100

    Extract git aliases

---
+++ git/aliases.zsh
@@ -0,0 +1,2 @@
+alias git-mate-diff="git diff --no-color | mate"
+alias git-log-pretty="git log --pretty=format:'%t %cr %an - %s'"
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,6 +7,3 @@ alias cd..="cd .."
 alias e="exit"
 alias ..="cd .."
 alias grep="grep --color"
-
-alias git-mate-diff="git diff --no-color | mate"
-alias git-log-pretty="git log --pretty=format:'%t %cr %an - %s'"
