eb8ebc1321d52239e8e5379832fc2bd3112814e1
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2011-12-21 21:08:56 +0100

    Fix prompt syntax (%{..%})

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,7 +8,7 @@ git_dirty_flag() {
   git status 2> /dev/null | grep -c : | awk '{if ($1 > 0) print "⚡"}'
 }
 
-export PROMPT=$'$fg_bold[green]%~%{$reset_color%}$fg_bold[blue]$(git_branch)%{$reset_color%}$fg_bold[red]$(git_dirty_flag)%{$reset_color%}› '
+export PROMPT=$'%{$fg_bold[green]%}%~%{$fg_bold[blue]%}$(git_branch)%{$fg_bold[red]%}$(git_dirty_flag)%{$reset_color%}› '
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
