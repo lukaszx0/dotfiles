e3762aa64e73c1324678eaecb53e16b0009091eb
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2012-02-22 21:32:50 +0100

    debug log

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,5 +1,6 @@
 alias restart="touch tmp/restart.txt"
 alias tlog="tail -f log/development.log"
+alias dlog="tail -f log/debug.log"
 alias dbmigrate='rake db:migrate db:test:clone'
 alias dbupgrade='rake db:migrate db:test:clone'
 
