f0edceb68707546a62fa3ba7ec13f8419c183f6e
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2012-07-29 12:00:09 +0200

    Add ip, flushdns aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,4 +8,11 @@ alias e="exit"
 alias ..="cd .."
 alias grep="grep --color"
 
+#Copy publickey to clipboard
 alias pubkey="cat ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+
+# Flush DNS cache
+alias flushdns="dscacheutil -flushcache"
