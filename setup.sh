65305ec4b36c4a0f319f46ad0e32b72f424864c5
Lukasz Strzalkowski <lukasz.strzalkowski@gmail.com>
2018-03-09 21:56:41 -0800

    update bare bone setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "TODO, trolololo"
+set -euf -o pipefail
+
+cloneDir=myconfig
+git clone --bare git@github.com:lukaszx0/dotfiles.git $HOME/.$cloneDir
+alias cfg="/usr/bin/git --git-dir=$HOME/.$cloneDir/ --work-tree=$HOME"
+cfg checkout
+
+# mkdir -p .config-backup && \
+# config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+# xargs -I{} mv {} .config-backup/{}
+
+cfg config --local status.showUntrackedFiles no
