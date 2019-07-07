b683465e6509b5c052aeae1f7d234d472d9c2340
Lukasz Strzalkowski <lukasz.strzalkowski@gmail.com>
2019-07-06 23:27:54 -0700

    hide readme and setup script (update cfg to dot)

--- .setup.sh
+++ .setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euf -o pipefail
 
-cloneDir=dotfiles
+cloneDir=.dotfiles
 git clone --bare git@github.com:lukaszx0/dotfiles.git $HOME/.$cloneDir
-alias cfg="/usr/bin/git --git-dir=$HOME/.$cloneDir/ --work-tree=$HOME"
-cfg checkout
+alias dot="/usr/bin/git --git-dir=$HOME/.$cloneDir/ --work-tree=$HOME"
+dot checkout
 
 # mkdir -p .config-backup && \
 # config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
 # xargs -I{} mv {} .config-backup/{}
 
-cfg config --local status.showUntrackedFiles no
+dot config --local status.showUntrackedFiles no
