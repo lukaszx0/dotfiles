fbb1f31c30af4123b0321ac142fa785b5594c895
Lukasz Strzalkowski <lukasz.strzalkowski@gmail.com>
2023-01-30 00:58:47 +0100

    use nbp for currency exchange rate

--- .bitbar/usd_rate.1h.sh
+++ .bitbar/usd_rate.1h.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PATH="/usr/local/bin:$PATH"
-RATE=$(curl -s https://currency-api.appspot.com/api/USD/PLN.json | jq .rate | xargs printf '%.*f\n' 2)
+RATE=$(curl -s "https://api.nbp.pl/api/exchangerates/rates/a/usd/last/?format=json" | jq '.rates[0].mid' | xargs printf '%.*f\n' 2)
 
 echo "USD: $RATE"
